ff1696b9ef9157aec1adfbbb0884a11b779848c2
CosmikArch <adityatiwari1880@gmail.com>
2026-01-07 14:23:32 +0530

    V0.2- Added a scoreboard in Rock Paper Scissors game (still no loop)

--- Mini game/rock_paper_scissors/rps_game.py
+++ Mini game/rock_paper_scissors/rps_game.py
@@ -1,11 +1,16 @@
 #  rock paper scissors
+#  version 0.2
 
 import random #  for choice computers can't think duh 🙄 
 import time #  for the countdown 🙂 
 
-Greeting = ["I foresee a future, where you lose!", "I foresee a future where victory is you destiny!", "You walked have upon the path of misry, now suffer!", "Your path may lead you to victory!","Feeling lucky, aren't you?", "I hope you don't regret your choices!"] #  i still haven't learned about lists and truples much (i believe i spelt that wrong )
+#  Score Variables
+RNG_score = 0
+player_score = 0
 
-Greet = random.choice(Greeting) #Dont' get scared now
+Greeting = ["I foresee a future, where you lose!", "I foresee a future where victory is you destiny!", "You walked have upon the path of misry, now suffer!", "Your path may lead you to victory!","Feeling lucky, aren't you?", "I hope you don't regret your choices!"] #  i still haven't learned about lists and tuples much.
+
+Greet = random.choice(Greeting) #Dont' get scared now 😈
 
 player = input("Enter your username:  ") #please don't use numbers 🥺
 
@@ -14,6 +19,8 @@ print(f"Greeting, {player}, I am RNG_The_Chad. {Greet}")
 choices = ["rock", "paper", "scissors"] #actual set of choices
 
 computer_choice = random.choice(choices) #to randomly pick ⛏️ a set choice 
+print("                                         ")
+
 
 print("Rock") #a count down for more realism
 time.sleep(0.5)
@@ -35,18 +42,29 @@ if player_choice == computer_choice:
 #  winning conditions
 elif player_choice == "rock" and computer_choice == "scissors":
   print("You win!🎊")
+  player_score += 1
 elif player_choice == "paper" and computer_choice == "rock":
   print("You win!🎉")
+  player_score += 1
 elif player_choice == "scissors" and computer_choice == "paper":
   print("You win!🎉") #winning conditions done ✅
+  player_score += 1
   
-  #  loosing conditions
+  #  losing conditions
 elif player_choice == "rock" and computer_choice == "paper":
   print("You lose! :(")
+  RNG_score += 1
 elif player_choice == "paper" and computer_choice == "scissors":
   print("You lose! :(")
+  RNG_score += 1
 elif player_choice == "scissors" and computer_choice == "rock":
   print("You lose! :(")
+  RNG_score += 1
   
 else:
   print(f"{player_choice} is not a valid choice! you are dumb 🗿")
+  player_score -= 1 #  penalty for trying to break the code! 😈
+print(f"""----------Score------------
+{player} - {player_score} 
+RNG_The_Chad - {RNG_score}
+-------------------------------""")
